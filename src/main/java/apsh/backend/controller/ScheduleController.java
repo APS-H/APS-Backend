f260234b303df398da375c2c9a41154db11a5ce2
HeBomou <paakmau@outlook.com>
2020-10-16 20:34:31 +0800

    style: 修复格式

--- src/main/java/apsh/backend/controller/ScheduleController.java
+++ src/main/java/apsh/backend/controller/ScheduleController.java
@@ -14,44 +14,45 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @RestController
 @RequestMapping(value = "/schedule")
 public class ScheduleController {
-    @GetMapping(value="/plan-table")
+    @GetMapping(value = "/plan-table")
     public List<SchedulePlanTableOrderVo> getPlanTable(@RequestParam Integer pageSize, @RequestParam Integer pageNum) {
         // TODO:
         return null;
     }
 
-    @GetMapping(value="/order-production-table")
-    public List<ScheduleOrderProductionTableRelationVo> getOrderProductionTable(@RequestParam Integer pageSize, @RequestParam Integer pageNum) {
+    @GetMapping(value = "/order-production-table")
+    public List<ScheduleOrderProductionTableRelationVo> getOrderProductionTable(@RequestParam Integer pageSize,
+            @RequestParam Integer pageNum) {
         // TODO:
         return null;
     }
 
-    @GetMapping(value="/production-table")
-    public List<ScheduleProductionTableProductionVo> getProductionTable(@RequestParam Integer pageSize, @RequestParam Integer pageNum) {
+    @GetMapping(value = "/production-table")
+    public List<ScheduleProductionTableProductionVo> getProductionTable(@RequestParam Integer pageSize,
+            @RequestParam Integer pageNum) {
         // TODO:
         return null;
     }
 
-    @GetMapping(value="/production-resource-table")
-    public List<ScheduleProductionResourceTableProductionVo> getProductionResourceTable(@RequestParam Integer pageSize, @RequestParam Integer pageNum) {
+    @GetMapping(value = "/production-resource-table")
+    public List<ScheduleProductionResourceTableProductionVo> getProductionResourceTable(@RequestParam Integer pageSize,
+            @RequestParam Integer pageNum) {
         // TODO:
         return null;
     }
 
-    @GetMapping(value="/table-export-url/{type}")
+    @GetMapping(value = "/table-export-url/{type}")
     public String getMethodName(@PathVariable Integer type) {
         // TODO:
         return null;
     }
 
-    @GetMapping(value="/table-export-url/all")
+    @GetMapping(value = "/table-export-url/all")
     public String getMethodName() {
         // TODO:
         return null;
     }
-    
 }
