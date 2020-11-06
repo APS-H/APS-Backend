9d0f899df1eabd48223c4abde3e5cf89f2a5f276
peihong.chen@transwarp.io <peihong.chen@transwarp.io>
2020-11-07 00:58:16 +0800

    test: simple test of LegacySystemService

--- src/main/java/apsh/backend/serviceImpl/LegacySystemServiceImpl.java
+++ src/main/java/apsh/backend/serviceImpl/LegacySystemServiceImpl.java
@@ -4,45 +4,40 @@ import apsh.backend.po.Equipment;
 import apsh.backend.po.Human;
 import apsh.backend.po.Order;
 import apsh.backend.service.LegacySystemService;
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Field;
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
-import org.apache.cxf.endpoint.Client;
-import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
 @Service
 public class LegacySystemServiceImpl implements LegacySystemService {
-    String orderServiceUrl = "http://localhost:8083/Order?wsdl";
-    String ERPServiceUrl = "http://localhost:8086/ERP?wsdl";
+
+    private final static String orderServiceUrl = "http://81.69.252.233:9001/order?wsdl";
+    private final static String erpServiceUrl = "http://81.69.252.233:9003/erp?wsdl";
 
     @Override
     public List<Order> getAllOrders() {
         JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
         Client client = dcf.createClient(orderServiceUrl);
-        Object[] objects = new Object[0];
         try {
             // invoke("方法名",参数1,参数2,参数3....);
-            objects = client.invoke("getOrderAll");
-            List<Object> orders = (List)(objects[0]);
-            List<Order> allOrders = orders.stream().map(o -> {
+            Object[] objects = client.invoke("getOrderAll");
+            List<Object> orders = (List) (objects[0]);
+            return orders.stream().map(o -> {
                 Order order = null;
                 try {
                     order = new Order(o);
-                } catch (NoSuchFieldException e) {
-                    e.printStackTrace();
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (ParseException e) {
+                } catch (NoSuchFieldException | IllegalAccessException | ParseException e) {
                     e.printStackTrace();
                 }
                 return order;
             }).collect(Collectors.toList());
-            return allOrders;
         } catch (java.lang.Exception e) {
             e.printStackTrace();
         }
@@ -52,35 +47,43 @@ public class LegacySystemServiceImpl implements LegacySystemService {
     @Override
     public List<Human> getAllHumans() {
         JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
-        Client client = dcf.createClient(ERPServiceUrl);
-        Object[] objects = new Object[0];
+        Client client = dcf.createClient(erpServiceUrl);
         try {
             // invoke("方法名",参数1,参数2,参数3....);
-            objects = client.invoke("getResourceAll");
-            List<Object> resources = (List)(objects[0]);
-
-            List<Human> allHumans = new ArrayList<Human>();
+            Object[] objects = client.invoke("getResourceAll");
+            List<Object> resources = (List) (objects[0]);
             Field f = resources.get(0).getClass().getDeclaredField("name");
             f.setAccessible(true);
-            for (Object o : resources) {
-                if (f.get(o).equals("班组")) {
-                    Human human = new Human(o);
-                    allHumans.add(human);
-                }
-            }
-            return allHumans;
+
+            return resources.parallelStream()
+                    .filter(r -> {
+                        try {
+                            return "班组".equals(f.get(r));
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
+                            return false;
+                        }
+                    })
+                    .map(r -> {
+                        try {
+                            return new Human(r);
+                        } catch (NoSuchFieldException | IllegalAccessException e) {
+                            e.printStackTrace();
+                            return null;
+                        }
+                    })
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
         } catch (java.lang.Exception e) {
             e.printStackTrace();
         }
-
-
         return null;
     }
 
     @Override
     public List<Equipment> getAllEquipments() {
         JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
-        Client client = dcf.createClient(ERPServiceUrl);
+        Client client = dcf.createClient(erpServiceUrl);
         Object[] objects = new Object[0];
         try {
             // invoke("方法名",参数1,参数2,参数3....);
@@ -88,13 +91,14 @@ public class LegacySystemServiceImpl implements LegacySystemService {
             List<Object> resources = (List) (objects[0]);
 
             List<Equipment> allEquipments = new ArrayList<Equipment>();
-            Field f = resources.get(0).getClass().getDeclaredField("name");
-            f.setAccessible(true);
-            Field ff = resources.get(0).getClass().getDeclaredField("count");
-            ff.setAccessible(true);
+            Field fieldName = resources.get(0).getClass().getDeclaredField("name");
+            fieldName.setAccessible(true);
+            Field fieldCount = resources.get(0).getClass().getDeclaredField("count");
+            fieldCount.setAccessible(true);
+
             for (Object o : resources) {
-                if (f.get(o).equals("线体") || f.get(o).equals("设备")) {
-                    int count = (int) ff.get(o);
+                if (fieldName.get(o).equals("线体") || fieldName.get(o).equals("设备")) {
+                    int count = (int) fieldCount.get(o);
                     for (int i = 0; i < count; i++) {
                         Equipment equipment = new Equipment(o);
                         allEquipments.add(equipment);
@@ -105,12 +109,6 @@ public class LegacySystemServiceImpl implements LegacySystemService {
         } catch (java.lang.Exception e) {
             e.printStackTrace();
         }
-
-
         return null;
-
     }
-
-
-
 }
