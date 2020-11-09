9fed8910253b28399f3f36076613f14b2cc59a8c
lightysand <724688247@qq.com>
2020-11-09 21:49:18 +0800

    feat:添加Schedule模块获取订单计划表与获取订单-生产单关系表接口

--- src/main/java/apsh/backend/vo/TaskInScheduleProductionTableProductionVo.java
+++ src/main/java/apsh/backend/vo/TaskInScheduleProductionTableProductionVo.java
@@ -1,5 +1,17 @@
 package apsh.backend.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskInScheduleProductionTableProductionVo {
-    // TODO: 生产任务
+    Integer id;
+    Integer productNum;          // 该子订单下产品数
+    Date startTime;  // 该子订单开始时间
+    Date endTime;
 }
